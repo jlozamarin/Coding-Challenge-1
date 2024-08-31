d98ec5369f96428712cab698c96bb016ee64ce32
Jennifer Loza-Marin <jenniferlozamarin@usf.edu>
2024-08-31 14:06:45 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "Brianna Deaubler";
+const employeeID = 012803;
+var isActive = true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
