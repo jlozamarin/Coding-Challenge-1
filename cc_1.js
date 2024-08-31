8932886e9febe58780ea7ca0d4024fb893ed8582
Jennifer Loza-Marin <jenniferlozamarin@usf.edu>
2024-08-31 15:06:39 -0400

    Task 3 - Financial Calculations

--- cc_1.js
+++ cc_1.js
@@ -20,3 +20,19 @@ const productDetails = {
 }
 
 console.log(products, productDetails);
+
+
+// Task 3: Assignment Operators
+
+let accountBalance = 5000; // intiial balance
+console.log("Initial balance is: $" + accountBalance); // Output: 5000
+accountBalance += 250;
+console.log("After adding 250, account balance is: $" + accountBalance); // Output: 5250
+accountBalance -= 600;
+console.log("After subtracting 600, account balance is: $" + accountBalance); // Output: 4650
+accountBalance *= 1.2;
+console.log("After multiplying by 1.2, account balance is: $" + accountBalance.toFixed(2)); // Output: 5580.00
+accountBalance /= 2;
+console.log("After dividing by 2, account balance is: $" + accountBalance.toFixed(2)); // Output: 2790.00
+accountBalance %= 50;
+console.log("After modulus operation, account balance is: $" + accountBalance.toFixed(2)); // Output: 40.00
