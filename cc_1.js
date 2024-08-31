486cc3bd95481b3c3b3c57dd89356fd0a94951ba
Jennifer Loza-Marin <jenniferlozamarin@usf.edu>
2024-08-31 14:30:55 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,16 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+
+
+
+// Task 2: Compound Data Types
+
+let products = ["Matte Lipstick","Liquid Foundation","Eyeshadow Palette"];
+const productDetails = {
+    name: products,
+    price: 15.00,28.00,35.00
+    inStock: yes,
+}
+
+console.log(products, productDetails);
